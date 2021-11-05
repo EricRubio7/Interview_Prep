1f39fd546c1def8e117835d63ec5136953ca4ce5
EricRubio7 <52763302+EricRubio7@users.noreply.github.com>
2021-11-05 16:11:02 -0500

    Update Two_Sum_Python_Brute_Force.py

--- LetCode/Easy/1_Two_Sum/Two_Sum_Python_Brute_Force.py
+++ LetCode/Easy/1_Two_Sum/Two_Sum_Python_Brute_Force.py
@@ -8,3 +8,11 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[j] == target - nums[i]:
                     return [i, j]
+                
+#Time complexity O(n^2): Because of the nested loop
+#Space complexity O(1): Because the space required does not depend on the size of the input array
+
+#Lessons: 
+#1) Arrays and nested loops will be useful for a quick brute force approach
+#2) Thinking of the problem as and algebra problem will give a quick solution: x + y = target
+#3) We can use range in python to create a sequence of numbers, and use it for iterations
